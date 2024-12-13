c549a18b8db6aa7ebd65cf8039010df56730703c
Ali Yousif Nayyef <64371717+AliNayyef@users.noreply.github.com>
2024-12-13 17:28:03 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Injection-Man',  
-    version='1.0.0',  
-    packages=find_packages(), 
+    name='Injection-Man',
+    version='1.0.0',
+    packages=find_packages(),
     install_requires=[],
-    python_requires=">=3.6",
+    include_package_data=True,
     entry_points={
         'console_scripts': [
-            'injection-man=injection_man.main:main',  
+            'injection-man=injection_man.main:main',
         ],
     },
     description='''
     Injecion-Man: XSS & SQL Injection Exploiter (XSS-SQLi-Exploit)
-    
+
     Description:
     The XSS & SQL Injection Exploiter is a Python-based tool designed for security researchers,
     ethical hackers, and penetration testers to identify and demonstrate vulnerabilities in web applications.
@@ -26,14 +26,14 @@ setup(
     SQL Injection (SQLi):
     SQL Injection vulnerabilities enable attackers to manipulate backend databases by injecting malicious SQL code into an application’s query. 
     This tool automates the process of identifying and exploiting SQLi weaknesses by submitting common SQL injection payloads to URL parameters.
-    
+
     Features:
     Multiple Payload Support:
     Includes a variety of pre-built XSS and SQLi payloads, such as:
 
     XSS payloads to execute malicious scripts in the victim’s browser.
     SQLi payloads to manipulate database queries and extract sensitive information.
-    
+
     ''',
     author='Ali Yousif Nayyef',
     author_email='ali.nayyef.contact@gmail.com',
